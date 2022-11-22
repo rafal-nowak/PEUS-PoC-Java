24b62b0aa9dd12e5a4dcd881b54c7bfaa74f69ef
rafal-nowak <contact@rafal-nowak.com>
2022-11-22 22:57:40 +0100

    initial commit

---
+++ src/test/java/com/example/peuspocjava/PeusPoCJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.peuspocjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeusPoCJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
